4700e4ac5b8af1c86688d6d1a8aaf5e86760116b
E.E <godmin@godmins-MacBook-Pro.local>
2016-08-19 11:49:33 -0400

    basic angular setup, first api, prelim results

--- pokesearch.app.js
+++ pokesearch.app.js
@@ -1,5 +1,23 @@
 var app = angular.module('pokesearch', [])
 
-app.controller('MainController', ['$scope', function($scope){
-	$scope.pokemon = "BULBASAUR!";
+app.controller('MainController', ['$scope', '$http', function($scope, $http){
+	$scope.$watch('search', function(){
+		if($scope.search === ""){
+			$scope.details = "No results.";
+		} else {
+			fetch();
+		}
+	})
+
+	$scope.search = "bulbasaur";
+
+	function fetch(){
+		$http.get("http://pokeapi.co/api/v2/pokemon/"+$scope.search+"/").then(function(response){
+			$scope.details = response.data;
+		});
+	}
+
+	$scope.select = function(){
+		this.setSelectionRange(0, this.value.length);
+	}
 }])
