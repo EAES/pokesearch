4fce5b78e5270bb644ea34165fcfe9a26ac09017
E.E <godmin@godmins-MacBook-Pro.local>
2016-08-21 11:04:31 -0400

    add loader and capture pokemon types in array to pull weaknesses

--- pokesearch.app.js
+++ pokesearch.app.js
@@ -14,6 +14,21 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
 	function fetch(){
 		$http.get("http://pokeapi.co/api/v2/pokemon/"+$scope.search+"/").then(function(response){
 			$scope.details = response.data;
+
+			$http.get("assets/typeweakness.json").then(function(response){
+				//capture pokemon types
+				var pokemonTypes = [];
+				for(type in $scope.details.types){
+					if ($scope.details.types.hasOwnProperty(type)) {
+						pokemonTypes.push($scope.details.types[type].type.name);
+					}
+					
+				}
+				console.log(pokemonTypes);
+				//search weakness list for each type
+				//return immunes, weaknesses, and strengths from weakness list
+			});
+
 		});
 	}
 
