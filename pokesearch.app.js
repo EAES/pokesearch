92b4ad786ff8d95682a26efd6266d940af2abecd
E.E <godmin@godmins-MacBook-Pro.local>
2016-08-22 11:57:39 -0400

    comments

--- pokesearch.app.js
+++ pokesearch.app.js
@@ -9,12 +9,15 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
 		}
 	})
 
+	//set default search param to first pokemon in pokedex order
 	$scope.search = "bulbasaur";
 
 	function fetch(){
+		//get basic pokemon data
 		$http.get("http://pokeapi.co/api/v2/pokemon/"+$scope.search+"/").then(function(response){
 			$scope.details = response.data;
 
+			//get weaknesses after fetching pokemon data
 			$http.get("assets/typeweakness.json").then(function(response){
 
 				var typeWeakness = response.data;
@@ -27,9 +30,11 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
 					
 				}
 
+				//create new weaknesses object
 				var currPokeWeaknesses = {};
 				$scope.typeEffectiveness = currPokeWeaknesses;
 
+				//create nested object with name, immunes, weaks, and strs
 				function captureWeaknesses(type){
 					for(var i=0; i < typeWeakness.length; i++){
 						if(type.toUpperCase() == typeWeakness[i].name.toUpperCase()){
@@ -42,6 +47,7 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
 					}
 				}
 
+				//get weakness for each current pokemon type
 				for(var i=0; i < pokemonTypes.length; i++){
 					captureWeaknesses(pokemonTypes[i]);
 				}
@@ -50,6 +56,7 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
 		});
 	}
 
+	//select all when clicking inside text input box
 	$scope.select = function(){
 		this.setSelectionRange(0, this.value.length);
 	}
