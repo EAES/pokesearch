675ec3c8efd92422d0eba0664e74f19d45293713
E.E <godmin@godmins-MacBook-Pro.local>
2016-08-26 16:17:48 -0400

    put app JS in IIFE, resolve all JSHINT warnings

--- pokesearch.app.js
+++ pokesearch.app.js
@@ -1,176 +1,190 @@
-var app = angular.module('pokesearch', [])
+/*global angular:true */
+/* exported scope */
 
-//TODO:
-	//hyphenated search fix (e.g. mr-mime, mime-jr)
-	//figure out evolution use/edge cases
-		//move learn use case
-		//eevee edge case
-		//rotom edge case
-		//weather use cases
-		//upside down edge case
-		//male/female use case
-		//alt forms use cases (wormadam)
-		//show mega evos if present
-	//speed up capture of evo chain
-	//cache results
+//IIFE
+(function(){
+	"use strict";
 
-//capitilize filter
-app.filter('capitalize', function() {
-  return function(input, scope) {
-    if (input!=null)
-    input = input.toLowerCase();
-    return input.substring(0,1).toUpperCase()+input.substring(1);
-  }
-});
+	var app = angular.module('pokesearch', []);
 
-//fixed number length filter
-app.filter('numLength', function() {
-  return function (n, len) {
-	var num = parseInt(n, 10);
-	len = parseInt(len, 10);
-	if (isNaN(num) || isNaN(len)) {
-	    return n;
-	}
-	num = ''+num;
-	while (num.length < len) {
-	    num = '0'+num;
-	}
-	return num;
-  }
-});
+	//TODO:
+		//hyphenated search fix (e.g. mr-mime, mime-jr)
+		//figure out evolution use/edge cases
+			//move learn use case
+			//eevee edge case
+			//rotom edge case
+			//weather use cases
+			//upside down edge case
+			//male/female use case
+			//branch evo use case
+			//alt forms use cases (wormadam)
+			//show mega evos if present
+		//speed up capture of evo chain
+		//cache results
 
-//display placeholder image if no pokemon sprite found
-app.directive('errSrc', function() {
-  return {
-    link: function(scope, element, attrs) {
-      element.bind('error', function() {
-        if (attrs.src != attrs.errSrc) {
-          attrs.$set('src', attrs.errSrc);
-        }
-      });
-    }
-  }
-});
+	//capitilize filter
+	app.filter('capitalize', function() {
+	  return function(input) {
+	    if (input!==null){
+	    	input = input.toLowerCase();
+		    return input.substring(0,1).toUpperCase()+input.substring(1);
+	    }
+	  };
+	});
 
-app.controller('MainController', ['$scope', '$http', function($scope, $http){
-	$scope.$watch('search', function(){
-		if($scope.search === ""){
-			$scope.details = "No results.";
-		} else {
-			fetch();
+	//fixed number length filter
+	app.filter('numLength', function() {
+	  return function (n, len) {
+		var num = parseInt(n, 10);
+		len = parseInt(len, 10);
+		if (isNaN(num) || isNaN(len)) {
+		    return n;
 		}
-	})
+		num = ''+num;
+		while (num.length < len) {
+		    num = '0'+num;
+		}
+		return num;
+	  };
+	});
 
-	//set default search param to first pokemon in pokedex order
-	$scope.search = "bulbasaur";
+	//display placeholder image if no pokemon sprite found
+	app.directive('errSrc', function() {
+	  return {
+	    link: function(scope, element, attrs) {
+	      element.bind('error', function() {
+	        if (attrs.src !== attrs.errSrc) {
+	          attrs.$set('src', attrs.errSrc);
+	        }
+	      });
+	    }
+	  };
+	});
 
-	function fetch(){
-
-		$scope.pokemonEvoChain = '';
-		$scope.serviceError = '';
-
-		//cature evo chain
-		$http.get("http://pokeapi.co/api/v2/pokemon-species/"+$scope.search+"/").then(function(response){
-			$scope.pokemonSpecies = response.data;
-			$http.get($scope.pokemonSpecies.evolution_chain.url).then(function(response){
-				
-				//thanks Ryan @ stackoverflow!
-				var evoChain = [];
-				var evoData = response.data.chain;
-				$scope.pokemonEvoDump = evoData;
-
-				do {
-				  var evoDetails = evoData['evolution_details'][0];
-
-				  evoChain.push({
-				    "species_name": evoData.species.name,
-				    "species_url": evoData.species.url,
-				    "min_level": !evoDetails ? 1 : evoDetails.min_level,
-				    "trigger_name": !evoDetails ? null : evoDetails.trigger.name,
-				    "item": !evoDetails ? null : evoDetails.item,
-				    "held_item": !evoDetails ? null : evoDetails.held_item,
-				    "overworld_rain": !evoDetails ? null : evoDetails.needs_overworld_rain,
-				    "happiness": !evoDetails ? null : evoDetails.min_happiness
-				  });
-
-				  evoData = evoData['evolves_to'][0];
-				} while (!!evoData && evoData.hasOwnProperty('evolves_to'));
-
-				$scope.pokemonEvoChain = evoChain;
-
-			});
+	app.controller('MainController', ['$scope', '$http', function($scope, $http){
+		$scope.$watch('search', function(){
+			if($scope.search === ""){
+				$scope.details = "No results.";
+			} else {
+				fetch();
+			}
 		});
 
-		$scope.pokemon = "";
+		//set default search param to first pokemon in pokedex order
+		$scope.search = "bulbasaur";
 
-		//get basic pokemon data
-		// TODO: don't connect each time, try and fetch main object and cache in localStorage
-		$http.get("http://pokeapi.co/api/v2/pokemon/"+$scope.search+"/").then(function(response){
-			
-			$scope.pokemon = response.data;
+		function fetch(){
 
-			//get weaknesses after fetching pokemon data
-			$http.get("assets/typeweakness.json").then(function(response){
+			$scope.pokemonEvoChain = '';
+			$scope.serviceError = '';
 
-				var typeWeakness = response.data;
-				var pokemonTypes = [];
+			//cature evo chain
+			$http.get("http://pokeapi.co/api/v2/pokemon-species/"+$scope.search+"/").then(function(response){
+				$scope.pokemonSpecies = response.data;
+				$http.get($scope.pokemonSpecies.evolution_chain.url).then(function(response){
+					
+					//thanks Ryan @ stackoverflow!
+					var evoChain = [];
+					var evoData = response.data.chain;
+					$scope.pokemonEvoDump = evoData;
 
-				for(type in $scope.pokemon.types){
-					if ($scope.pokemon.types.hasOwnProperty(type)) {
-						pokemonTypes.push($scope.pokemon.types[type].type.name);
-					}
-				}
+					do {
+					  var evoDetails = evoData.evolution_details[0];
 
-				//create new weaknesses object
-				var currPokeWeaknesses = {};
-				$scope.typeEffectiveness = currPokeWeaknesses;
+					  evoChain.push({
+					    "species_name": evoData.species.name,
+					    "species_url": evoData.species.url,
+					    "min_level": !evoDetails ? 1 : evoDetails.min_level,
+					    "trigger_name": !evoDetails ? null : evoDetails.trigger.name,
+					    "item": !evoDetails ? null : evoDetails.item,
+					    "held_item": !evoDetails ? null : evoDetails.held_item,
+					    "overworld_rain": !evoDetails ? null : evoDetails.needs_overworld_rain,
+					    "happiness": !evoDetails ? null : evoDetails.min_happiness
+					  });
 
-				//create nested object with name, immunes, weaks, and strs
-				function captureWeaknesses(type){
-					for(var i=0; i < typeWeakness.length; i++){
-						if(type.toUpperCase() == typeWeakness[i].name.toUpperCase()){
-							currPokeWeaknesses[i] = {};
-							currPokeWeaknesses[i]["name"] = typeWeakness[i].name;
-							currPokeWeaknesses[i]["immunes"] = typeWeakness[i].immunes.join(', ');
-							currPokeWeaknesses[i]["weaknesses"] = typeWeakness[i].weaknesses.join(', ');
-							currPokeWeaknesses[i]["strengths"] = typeWeakness[i].strengths.join(', ');
-						}
-					}
-				}
+					  evoData = evoData.evolves_to[0];
+					} while (!!evoData && evoData.hasOwnProperty('evolves_to'));
 
-				//get weakness for each current pokemon type
-				for(var i=0; i < pokemonTypes.length; i++){
-					captureWeaknesses(pokemonTypes[i]);
-				}
+					$scope.pokemonEvoChain = evoChain;
+
+				});
 			});
 
-		},
-		//error handling
-		function(response) {
-			if(response.status == 404){
-				$scope.serviceError = "Sorry, could not find pokemon you requested.";
-			} else if(response.status == 504){
-				$scope.serviceError = "Sorry, service is currently not available. Please check back later."
-			} else if(response.status !== 200){
-				$scope.serviceError = "Unknown error, please try again later."
+			$scope.pokemon = "";
+
+			//get basic pokemon data
+			// TODO: don't connect each time, try and fetch main object and cache in localStorage
+			$http.get("http://pokeapi.co/api/v2/pokemon/"+$scope.search+"/").then(function(response){
+				
+				$scope.pokemon = response.data;
+
+				//get weaknesses after fetching pokemon data
+				$http.get("assets/typeweakness.json").then(function(response){
+
+					var typeWeakness = response.data;
+					var pokemonTypes = [];
+
+					for(var type in $scope.pokemon.types){
+						if ($scope.pokemon.types.hasOwnProperty(type)) {
+							pokemonTypes.push($scope.pokemon.types[type].type.name);
+						}
+					}
+
+					//create new weaknesses object
+					var currPokeWeaknesses = {};
+					$scope.typeEffectiveness = currPokeWeaknesses;
+
+					//create nested object with name, immunes, weaks, and strs
+					function captureWeaknesses(type){
+						for(var i=0; i < typeWeakness.length; i++){
+							if(type.toUpperCase() === typeWeakness[i].name.toUpperCase()){
+								currPokeWeaknesses[i] = {};
+								currPokeWeaknesses[i].name = typeWeakness[i].name;
+								currPokeWeaknesses[i].immunes = typeWeakness[i].immunes.join(', ');
+								currPokeWeaknesses[i].weaknesses = typeWeakness[i].weaknesses.join(', ');
+								currPokeWeaknesses[i].strengths = typeWeakness[i].strengths.join(', ');
+							}
+						}
+					}
+
+					//get weakness for each current pokemon type
+					for(var i=0; i < pokemonTypes.length; i++){
+						captureWeaknesses(pokemonTypes[i]);
+					}
+				});
+
+			},
+			//error handling
+			function(response) {
+				if(response.status === 404){
+					$scope.serviceError = "Sorry, could not find pokemon you requested.";
+				} else if(response.status === 504){
+					$scope.serviceError = "Sorry, service is currently not available. Please check back later.";
+				} else if(response.status !== 200){
+					$scope.serviceError = "Unknown error, please try again later.";
+				}
 			}
+
+			);
 		}
 
-		);
-	}
+		$scope.getPokemonIdFromUrl = function(url){
+			url = url.slice(0, -1);
+			url = url.substring(url.lastIndexOf('/')+1);
+			return url;
+		};
 
-	$scope.getPokemonIdFromUrl = function(url){
-		url = url.slice(0, -1);
-		url = url.substring(url.lastIndexOf('/')+1);
-		return url;
-	}
+		$scope.changePokemon = function(newSearch){
+			$scope.search = newSearch;
+		};
 
-	$scope.changePokemon = function(newSearch){
-		$scope.search = newSearch;
-	}
+		//select all when clicking inside text input box
+		$scope.select = function(){
+			this.setSelectionRange(0, this.value.length);
+		};
 
-	//select all when clicking inside text input box
-	$scope.select = function(){
-		this.setSelectionRange(0, this.value.length);
-	}
-}])
+	}]);
+
+//end IIFE	
+})();
+
